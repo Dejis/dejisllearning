ebb7662a8fd6a8ef32c9ad606d60e979a69bcf54
OsarenmwindaOhuimumwenJoshua <jositron1@outlook.com>
2022-09-14 19:27:07 +0100

    Initial commit

---
+++ dejisllearning/dejisllearning/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse('Welcome to Dejisl Learning school! I am so glad archieving this milestone. Would you not rather learn code with me here?')
---
+++ dejislpro1.py
@@ -0,0 +1,4 @@
+print("Welcome to De Jinto Ideal Solution Limited")
+
+import django
+print(django.get_version())        
